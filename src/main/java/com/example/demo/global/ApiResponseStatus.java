f2e2ab9fc2c8d90c4b5f55fc5262d0db6c5c8fea
Sasha <106591445+kchaeys2@users.noreply.github.com>
2024-01-31 22:24:30 +0900

    feat($ApiResponseStatus) : WRONG 상태 추가

--- src/main/java/com/example/demo/global/ApiResponseStatus.java
+++ src/main/java/com/example/demo/global/ApiResponseStatus.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ApiResponseStatus {
     SUCCESS(200, "API 요청이 성공했습니다."),
+
+    WRONG(201,"불가능한 요청입니다."),
     FAIL(500, "에러가 발생했습니다.");
 
     private final int code;
