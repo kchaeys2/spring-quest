c12b85d56f95bf530cb9583e8e88d0ee056e886e
Sasha <106591445+kchaeys2@users.noreply.github.com>
2024-01-31 22:08:44 +0900

    feat($UserService) : 유저 전체 조회 API 생성

--- src/main/java/com/example/demo/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -1,9 +1,14 @@
 package com.example.demo.domain.user.service;
 
 import com.example.demo.domain.user.dto.response.InitUserResponse.UserData;
+import com.example.demo.domain.user.dto.response.UserPageResponse;
+import com.example.demo.domain.user.dto.response.UserResponse;
 import com.example.demo.domain.user.entity.User;
 import com.example.demo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,4 +28,13 @@ public class UserService {
                 .forEach(userRepository::save);
     }
 
+    public UserPageResponse findUsers(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<User> userPage = userRepository.findAll(pageable);
+
+        List<UserResponse> userResponses = userPage.getContent().stream()
+                .map(User::createUserResponse)
+                .toList();
+        return new UserPageResponse((int) userPage.getTotalElements(),userPage.getTotalPages(),userResponses);
+    }
 }
