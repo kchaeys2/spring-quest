ce4542ff10ca28b1368f8ff10b7517c7f17f15c4
Sasha <106591445+kchaeys2@users.noreply.github.com>
2024-02-03 20:40:39 +0900

    refactor : createAt,updateAt 이름 createdAt,updatedAt으로 변경

--- src/main/java/com/example/demo/domain/room/dto/response/RoomResponse.java
+++ src/main/java/com/example/demo/domain/room/dto/response/RoomResponse.java
@@ -8,14 +8,14 @@ import lombok.Getter;
 @Getter
 public class RoomResponse extends RoomsResponse {
     @Schema(description = "생성 일자", pattern = "yyyy-MM-dd HH:mm:ss", example = "2024-02-20 15:35:00")
-    private final String createAt;
+    private final String createdAt;
     @Schema(description = "수정 일자", pattern = "yyyy-MM-dd HH:mm:ss", example = "2024-02-20 15:35:00")
-    private final String updateAt;
+    private final String updatedAt;
 
     public RoomResponse(int id, String title, int hostId, RoomType roomType, RoomStatus status,
-                        String createAt, String updateAt) {
+                        String createAt, String updatedAt) {
         super(id, title, hostId, roomType, status);
-        this.createAt = createAt;
-        this.updateAt = updateAt;
+        this.createdAt = createAt;
+        this.updatedAt = updatedAt;
     }
 }
